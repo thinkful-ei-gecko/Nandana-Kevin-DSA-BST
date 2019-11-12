3435491df03a797edc3782d6b3d4646b229e7205
Kevin Wei <chenghaowei@gmail.com>
2019-11-12 12:14:36 -0500

    complete up to #4

---
+++ BST.js
@@ -0,0 +1,127 @@
+class BST {
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  display() {
+    if (this.key === null) {
+      return;
+    }
+
+    console.log({
+      key: this.key,
+      parent: this.parent ? this.parent.key : null,
+      left: this.left ? this.left.key : null,
+      right: this.right ? this.right.key : null
+    });
+
+    if (this.left !== null) {
+      this.left.display();
+    }
+
+    if (this.right !== null) {
+      this.right.display();
+    }
+  }
+
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    } else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BST(key, value, this);
+      } else {
+        this.left.insert(key, value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(key, value, this);
+      } else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    if (this.key === key) {
+      console.log(`Found key: ${key} with value: ${this.value}`);
+      return this.value;
+    } else if (key < this.key && this.left) {
+      return this.left.find(key);
+    } else if (key > this.key && this.right) {
+      return this.right.find(key);
+    } else {
+      throw new Error("Key Error");
+    }
+  }
+
+  remove(key) {
+    if (this.key === key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      } else if (this.left) {
+        /* If the node only has a left child, 
+           then you replace the node with its left child */
+        this._replaceWith(this.left);
+      } else if (this.right) {
+        /* And similarly if the node only has a right child 
+           then you replace it with its right child */
+        this._replaceWith(this.right);
+      } else {
+        /* If the node has no children then
+           simply remove it and any references to it 
+           by calling "this._replaceWith(null)" */
+        this._replaceWith(null);
+      }
+    } else if (key < this.key && this.left) {
+      this.left.remove(key);
+    } else if (key > this.key && this.right) {
+      this.right.remove(key);
+    } else {
+      throw new Error("Key Error");
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this === this.parent.left) {
+        this.parent.left = node;
+      } else if (this === this.parent.right) {
+        this.parent.right = node;
+      }
+
+      if (node) {
+        node.parent = this.parent;
+      }
+    } else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      } else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
+  }
+}
+
+module.exports = BST;
---
+++ Drills.js
@@ -0,0 +1,35 @@
+const BST = require("./BST");
+
+function main() {
+  let bst1 = new BST();
+  let insertVals = [3, 1, 4, 6, 9, 2, 5, 7];
+  for (let i = 0; i < insertVals.length; i++) {
+    bst1.insert(insertVals[i]);
+  }
+  // bst1.display();
+
+  let bst2 = new BST();
+  let chars = "EASYQUESTION".split("");
+  for (let i = 0; i < chars.length; i++) {
+    bst2.insert(chars[i]);
+  }
+  // bst2.display();
+
+  let bst3 = new BST(0, 1);
+  let keys = [1, 2, 3, 4, 5, 6, 7];
+  for (let i = 0; i < keys.length; i++) {
+    bst3.insert(keys[i], 2);
+  }
+  // console.log(tree(bst3));
+}
+main();
+
+// returns sum of all values in the BST (if child nodes have non-null value)
+// O(2^N)
+function tree(t) {
+  if (!t) {
+    return 0;
+  }
+  return tree(t.left) + t.value + tree(t.right);
+}
+
